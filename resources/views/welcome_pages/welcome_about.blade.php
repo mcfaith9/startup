7cfd9605d35a583477f75d01b0ba419cf3fd54b8
lelis.markanthony@yahoo.com <lelis.markanthony@yahoo.com>
2018-03-03 16:38:32 +0800

    about 3rd commit

--- resources/views/welcome_pages/welcome_about.blade.php
+++ resources/views/welcome_pages/welcome_about.blade.php
@@ -108,7 +108,7 @@
         <div class="content-box">
             <div class="active-content">
                 <div class="home-page content-wrapper">
-                    <p>Hello, We are Team Pila. In this website, you will be knowledgeable on how to aquire legal documents and id's that you might need on any purposes. With this site you can search and locate the nearest office and facilities where you can transact with regards to your legal documents and id's. <a href="#" class="btnInline nav-btn" data-target-class="about-page">about us</a>
+                    <p>Hello, We are <a href="#" class="btnInline"> Team Pila </a>. In this website, you will be knowledgeable on how to aquire legal documents and id's that you might need on any purposes. With this site you can search and locate the nearest office and facilities where you can transact with regards to your legal documents and id's. <a href="#" class="btnInline nav-btn" data-target-class="about-page">about us</a>
                      and our <a href="#" class="btnInline nav-btn" data-target-class="projects-page">recent projects</a>. You can also <a href="#" class="btnInline nav-btn" data-target-class="contact-page">contact us</a>, using various channels of communication.</p>
                 </div> <!-- end of home page -->
             </div> <!-- end of active content -->                
@@ -118,7 +118,7 @@
     </div> <!-- end of container -->        
     <div class="hidden-content">
         <div class="about-page content-wrapper">
-            <p>So we are team Pila, first and foremost we are compose of three persona namely: McFaith, Stephen and Macky, we are the developer of this site. It was our passion for several years and until now.</p>
+            <p>So we are <a href="#" class="btnInline"> Team Pila </a>, first and foremost we are compose of three persona namely: McFaith, Stephen and Macky, we are the developer of this site. It was our passion for several years and until now.</p>
             <p>McFaith came from City of Naga, Cebu, which is in the Philippines while Stephen and Macky came from Minglanilla, Cebu which is also in the Philippines. We collaborate our ideas like to create something beautiful looking and helpful to the community, using each imagination. We prefer functionality over estetics, when it comes to software design.</p>
             <p>Other things that we keep close to our heart is playing internet games like "dota2, ros", break dancing in which McFaith loves to do and playing basketball.</p>
             <p>Now that you know something about us, you can view our <a href="#" class="btnInline nav-btn" data-target-class="projects-page">recent projects</a>, <a href="#" class="btnInline nav-btn" data-target-class="contact-page">contact me</a> or return to <a href="#" class="btnInline nav-btn" data-target-class="home-page">home page</a>.</p>
@@ -128,13 +128,13 @@
             <p>Here you will find our most recent projects.</p>
             <p>Our last project was <a href="#" class="btnInline">Epets website</a>. We tried to get the main message and purpose of the site is selling pet food and accessories while using light background colors and bold font with dark colors.</p>
             <p>The other project i want to tell you about is <a href="#" class="btnInline">Questionaire System</a>. This system automate a questionaire given by a teacher to the student, so it is not surprising that the main color is pink. I also used a lot of images in this app, because it keeps the desing balanced.</p>
-            <p>And the last project is <a href="#" class="btnInline">Alpha Company website</a>. Because it is a banking institution, i had to keep the desing professional. I used dark, bold colors and kept saturated colors to minimum in this desing.</p>
-            <p>If you like my work and are interested in working with me, why won't you <a href="#" class="btnInline nav-btn" data-target-class="contact-page">contact me</a>. You can also return to <a href="#" class="btnInline nav-btn" data-target-class="home-page">home page</a>.</p>
+            <p>And the last project is <a href="#" class="btnInline">School website</a>. Because it is an institution, we had to keep the desing professional. We used dark, bold colors and kept saturated colors to minimum in this desing.</p>
+            <p>If you like our work and are interested in working with us, why won't you <a href="#" class="btnInline nav-btn" data-target-class="contact-page">contact us</a>. You can also return to <a href="#" class="btnInline nav-btn" data-target-class="home-page">home page</a>.</p>
         </div> <!-- end of projects page -->
         
         <div class="contact-page content-wrapper">
-            <p>As i said, i am from London, where i have a little office on <a target="_blank" href="https://www.google.com/maps/place/236+Oxford+St,+London+W1D+2LT,+UK/@51.5153975,-0.1413229,17z/data=!3m1!4b1!4m2!3m1!1s0x48761ad556c55073:0x3a5a72d8f000b8f4" class="btnInline">Oxford street 236</a>.</p>
-            <p>If you have any questions, please contact me. I am on basically every social network you can imagine - <a href="#" class="btn facebook">facebook</a>, <a href="#" class="btnInline twitter">twitter</a>, <a href="#" class="btnInline instagram">instagram</a>. So contacting me should not be a problem.</p>
+            <p>As we said, we came  from the Philippines, where we inspire this project in behalf of our school <a target="_blank" href="https://www.google.com.ph/maps/place/South+Poblacion,+City+of+Naga,+Cebu/@10.2019481,123.7501173,16z/data=!4m5!3m4!1s0x33a9785459bad58d:0xfb8b45e305069e6a!8m2!3d10.2017431!4d123.7559628" class="btnInline">Professional Academy of the Philippines</a>.</p>
+            <p>If you have any questions, please contact us. We are on basically every social network you can imagine - <a href="#" class="btn facebook">TeamPila@facebook.com</a>, <a href="#" class="btnInline twitter">TeamPila@twitter.com</a>, <a href="#" class="btnInline instagram">TeamPila@instagram.com</a>. So contacting us should not be a problem.</p>
             <p>You can also return to <a href="#" class="btnInline nav-btn" data-target-class="home-page">home page</a>.</p>
         </div> <!-- end of contact page -->
     </div> <!-- end of hidden content -->
